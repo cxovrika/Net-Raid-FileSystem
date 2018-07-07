fe2da771f0f0e4c77e10178b150a971d49f03f23
cxovrika <atskh15@freeuni.edu.ge>
2018-07-07 14:10:28 +0400

    Added basic code that uses sockets in Practice

---
+++ Practice/Socket_practice/Basic/tcp_client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+
+int main() {
+
+  // socket creation
+  int network_socket;
+  network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // address creation for socket (where to connect)
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(9002); //ITS OVER 9000!!!!
+  server_address.sin_addr.s_addr = INADDR_ANY;
+
+  int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+  // 0 means successful connection, -1 means failure(error number stored in errno)
+  if (connection_status == -1) {
+    printf("There was an error making a connection.\n");
+  }
+    else
+  {
+    // receiving information and printing
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+    printf("server response: %s\n", server_response);
+  }
+
+  // closing socket
+  close(network_socket);
+
+  return 0;
+}
